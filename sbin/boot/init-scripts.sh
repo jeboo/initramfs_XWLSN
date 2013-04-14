17785cb9247f4292e7f8c15f0716d9a5f9683424
PhilZ-cwm6 <phytowardt@gmail.com>
2013-04-14 15:03:23 +0300

    Do not run init.d twice!

--- sbin/boot/init-scripts.sh
+++ sbin/boot/init-scripts.sh
@@ -3,9 +3,9 @@
 # defines init.d and customboot.sh files location
 # enables init.d & customboot.sh
 
-if /sbin/busybox [ -d /system/etc/init.d ]; then
-  /sbin/busybox run-parts /system/etc/init.d
-fi;
+#if /sbin/busybox [ -d /system/etc/init.d ]; then
+#  /sbin/busybox run-parts /system/etc/init.d
+#fi;
 
 if /sbin/busybox [ -f /system/bin/customboot.sh ]; then
   /sbin/busybox sh /system/bin/customboot.sh;
